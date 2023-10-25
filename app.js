ecd36047ce34c3e470144489b5b9fd20e9ce80a7
anshulmann11 <anshulmann012@gmail.com>
2023-10-25 11:49:27 +0530

    appjs setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define routes and middleware here
+const cors = require('cors');
+
+// Middleware for JSON parsing
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
